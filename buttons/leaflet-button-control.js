544c2fc4f6b08aaf822591cd6a04e621a2bde0f3
Thejesh GN <i@thejeshgn.com>
2014-07-25 16:10:56 +0530

    added font awesome for all the icons required

--- buttons/leaflet-button-control.js
+++ buttons/leaflet-button-control.js
@@ -1,113 +1,46 @@
+L.Control.EasyButtons = L.Control.extend({
+    options: {
+        position: 'topleft',
+        title: '',
+        intentedIcon: 'fa-circle-o'
+    },
 
-L.Control.Button = L.Control.extend({
-  options: {
-    position: 'topleft'
-  },
-  initialize: function (options) {
-    this._button = {};
-    this.setButton(options);
-  },
+    onAdd: function () {
+        var container = L.DomUtil.create('div', 'leaflet-bar leaflet-control home_button');
 
-  onAdd: function (map) {
-    this._map = map;
-    var container = L.DomUtil.create('div', 'leaflet-control-button');
-	
-    this._container = container;
+        this.link = L.DomUtil.create('a', 'leaflet-bar-part', container);
+        L.DomUtil.create('i', 'fa fa-lg ' + this.options.intentedIcon , this.link);
+        this.link.href = '#';
+
+        L.DomEvent.on(this.link, 'click', this._click, this);
+        this.link.title = this.options.title;
+
+        return container;
+    },
     
-    this._update();
-    return this._container;
-  },
-
-  onRemove: function (map) {
-  },
-
-  setButton: function (options) {
-    var button = {
-      'text': options.text,                 //string
-      'iconUrl': options.iconUrl,           //string
-      'onClick': options.onClick,           //callback function
-      'hideText': options.hideText,         //forced bool
-      'maxWidth': options.maxWidth || 70,     //number
-      'doToggle': options.toggle,			//bool
-      'toggleStatus': false					//bool
-    };
-
-    this._button = button;
-    this._update();
-  },
+    intendedFunction: function(){ alert('no function selected');},
   
-  getText: function () {
-      return this._button.text;
-  },
-  
-  getIconUrl: function () {
-  	return this._button.iconUrl;
-  },
-  
-  destroy: function () {
-  	this._button = {};
-  	this._update();
-  },
-  
-  toggle: function (e) {
-  	if(typeof e === 'boolean'){
-  		this._button.toggleStatus = e;
-  	}
-  	else{
-  		this._button.toggleStatus = !this._button.toggleStatus;
-  	}
-  	this._update();
-  },
-  
-  _update: function () {
-    if (!this._map) {
-      return;
-    }
-
-    this._container.innerHTML = '';
-    this._makeButton(this._button);
- 
-  },
-
-  _makeButton: function (button) {
-    var newButton = L.DomUtil.create('div', 'leaflet-bar leaflet-buttons-control-button home_button ', this._container);
-    if(button.toggleStatus)
-    	L.DomUtil.addClass(newButton,'leaflet-buttons-control-toggleon');
-        
-    var image = L.DomUtil.create('img', 'leaflet-buttons-control-img home_button_img', newButton);
-    image.setAttribute('src',button.iconUrl);
-    
-    if(button.text !== ''){
-
-      L.DomUtil.create('br','',newButton);  //there must be a better way
-
-      var span = L.DomUtil.create('span', 'leaflet-buttons-control-text', newButton);
-      var text = document.createTextNode(button.text);  //is there an L.DomUtil for this?
-      span.appendChild(text);
-      if(button.hideText)
-        L.DomUtil.addClass(span,'leaflet-buttons-control-text-hide');
-    }
-
-    L.DomEvent
-      .addListener(newButton, 'click', L.DomEvent.stop)
-      .addListener(newButton, 'click', button.onClick,this)
-      .addListener(newButton, 'click', this._clicked,this);
-    L.DomEvent.disableClickPropagation(newButton);
-    return newButton;
-
-  },
-  
-  _clicked: function () {  //'this' refers to button
-  	if(this._button.doToggle){
-  		if(this._button.toggleStatus) {	//currently true, remove class
-  			L.DomUtil.removeClass(this._container.childNodes[0],'leaflet-buttons-control-toggleon');
-  		}
-  		else{
-  			L.DomUtil.addClass(this._container.childNodes[0],'leaflet-buttons-control-toggleon');
-  		}
-  		this.toggle();
-  	}
-  	return;
-  }
-
+    _click: function (e) {
+        L.DomEvent.stopPropagation(e);
+        L.DomEvent.preventDefault(e);
+        this.intendedFunction();
+    },
 });
+
+L.easyButton = function( btnIcon , btnFunction , btnTitle , btnMap ) {
+  var newControl = new L.Control.EasyButtons;
+  if (btnIcon) newControl.options.intentedIcon = btnIcon;
+  
+  if ( typeof btnFunction === 'function'){
+    newControl.intendedFunction = btnFunction;
+  } 
+  
+  if (btnTitle) newControl.options.title = btnTitle;
+  
+  if ( btnMap ){
+    newControl.addTo(btnMap);
+  } else {
+    newControl.addTo(map);
+  }
+  return newControl;
+};
